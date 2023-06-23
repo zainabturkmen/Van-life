1edf631c71ab403721a03d63a0a361962c00476e
Zainab <zainabturkmen247@gmail.com>
2023-06-23 15:00:40 +0430

    init

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-import bgImg from "../assets/images/about-hero.png"
+import bgImg from "../../src/assets/images/about-hero.png"
+// import bgImg from "../assets/images/about-hero.png"
 import { Link } from "react-router-dom"
 
 export default function About() {
