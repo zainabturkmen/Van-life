4c8f5fdebe49dfde428dae4abe0bddedfd0b4755
zainabturkmen <zainabturkmen7@gmail.com>
2024-01-03 19:50:16 +0430

    init

--- src/main.jsx
+++ src/main.jsx
@@ -25,7 +25,7 @@ function App() {
   /**
    * Challenge: Create the AuthRequired Layout Route to protect
    * all the /host routes.
-   * 
+   *   
    * For now, just use `const authenticated = false`
    * to determine the authenticated status of the user, and
    * either send them to the /login route, or render the Outlet
